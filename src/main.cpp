3ad6fffe320d26693bd6efdaba30991caf84f970
Lucas Wilson <lkwilson96@gmail.com>
2021-01-22 00:31:49 -0700

    Add wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <wifi_mgr.h>
+#include <light_mgr.h>
+#include <web_server.h>
+
+#include "test.html"
 
 void setup() {
   // put your setup code here, to run once:
---
+++ src/wifi_mgr.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <WiFiManager.h>
