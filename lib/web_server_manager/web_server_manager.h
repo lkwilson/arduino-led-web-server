4fd5d3f41a1ddb5f88ad08a61181036600ee6768
Lucas Wilson <lkwilson96@gmail.com>
2021-01-30 13:12:41 -0700

    Enable SPIFFS

--- lib/web_server_manager/web_server_manager.h
+++ lib/web_server_manager/web_server_manager.h
@@ -6,8 +6,19 @@ struct WebServerManager {
 
   public: // api
     void setup() {
-      m_server.begin();
+      if (!SPIFFS.begin()) {
+        Serial.println("Unable to mount SPIFFS directory");
+      }
       m_server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+      m_server.onNotFound([this](AsyncWebServerRequest *request){ this->on_not_found(request); });
+
+      m_server.begin();
+    }
+
+  private: // handlers
+    void on_not_found(AsyncWebServerRequest *request) {
+      Serial.println("Received not found request. Responding with 404.");
+      request->send(404);
     }
 
   private: // members
