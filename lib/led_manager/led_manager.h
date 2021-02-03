db790511ca29faea45e2ebab5bc19bfc335f15be
Lucas Wilson <lkwilson96@gmail.com>
2021-02-02 19:53:50 -0700

    Led Manager builds

--- lib/led_manager/led_manager.h
+++ lib/led_manager/led_manager.h
@@ -4,148 +4,259 @@
 #include <FastLED.h>
 #include <qr_util.h>
 
-struct Fader {
-  public: // ctor
-    Fader() : m_active(false) {}
+enum class ColorStateEnum {
+  IDLE,
+  FADE,
+  FOLLOW,
+};
 
-  public: // api
-    void set(
-        const millis_t start_time,
-        const millis_t duration,
-        const CRGB& current_color,
-        const uint8_t r,
-        const uint8_t g,
-        const uint8_t b) {
-      m_active = true;
-      m_base_color = current_color;
-      m_target_color.r = r;
-      m_target_color.g = g;
-      m_target_color.b = b;
+struct FollowState {
+  void update(const millis_t current_time, CRGB& led)
+  {
 
-      m_r_offset = r;
-      m_r_offset -= current_color.r;
+  }
+};
 
-      m_g_offset = g;
-      m_g_offset -= current_color.g;
+struct FadeLogic {
+  void set(const int start, const int end) {
+    m_start = start;
+    m_end = end;
+    m_offset = end - start;
+  }
 
-      m_b_offset = b;
-      m_b_offset -= current_color.b;
+  int get(const double progress) const {
+    return m_start + m_offset * progress;
+  }
 
-      m_start_time = start_time;
-      m_duration = duration;
+  int get_end() const {
+    return m_end;
+  }
+
+  int m_start;
+  int m_end;
+  int m_offset;
+};
+
+struct FadeState {
+  void set(
+      const millis_t init_time,
+      const millis_t delay_duration,
+      const millis_t fade_duration,
+      CRGB& led,
+      const uint8_t target_red,
+      const uint8_t target_green,
+      const uint8_t target_blue)
+  {
+    m_init_time = init_time;
+    m_delay_duration = delay_duration;
+    m_fade_duration = fade_duration;
+    m_red.set(led.red, target_red);
+    m_green.set(led.green, target_green);
+    m_blue.set(led.blue, target_blue);
+
+  }
+
+  // returns true when done
+  bool update(const millis_t current_time, CRGB& led) const {
+    const auto delay_interval = current_time - m_init_time;
+    const auto fade_interval = delay_interval - m_delay_duration;
+    if (delay_interval <= m_delay_duration) {
+      // Do nothing
+      return false;
+    } else if (fade_interval < m_fade_duration) {
+      // 0 < fade_interval < m_fade_duration
+      const auto progress = static_cast<double>(fade_interval) / m_fade_duration;
+      led.red = m_red.get(progress);
+      led.green = m_green.get(progress);
+      led.blue = m_blue.get(progress);
+      return false;
+    } else {
+      led.red = m_red.get_end();
+      led.green = m_green.get_end();
+      led.blue = m_blue.get_end();
+      return true;
     }
+  }
 
-    bool is_active() const {
-      return m_active;
-    }
+  millis_t m_init_time;
+  millis_t m_delay_duration;
+  millis_t m_fade_duration;
 
-    bool update(const millis_t current_time, CRGB& led) {
-      const auto passed_time = current_time - m_start_time;
-      if (passed_time < m_duration) {
-        const auto progress = static_cast<double>(passed_time) / m_duration;
-        const auto transformed_progress = transform_progress(progress);
-        const auto r = m_base_color.r + static_cast<int>(transformed_progress * m_r_offset);
-        const auto g = m_base_color.g + static_cast<int>(transformed_progress * m_g_offset);
-        const auto b = m_base_color.b + static_cast<int>(transformed_progress * m_b_offset);
-        Serial.printf("Updating to %d %d %d\n", r, g, b);
-        led.r = r;
-        led.g = g;
-        led.b = b;
-      } else {
-        m_active = false;
-        led = m_target_color;
+  FadeLogic m_red;
+  FadeLogic m_green;
+  FadeLogic m_blue;
+
+  ColorStateEnum m_prev_state;
+};
+
+struct ColorState {
+  ColorState() : m_state(ColorStateEnum::IDLE) {}
+
+  ColorStateEnum update(const millis_t current_time, CRGB& led) {
+    switch (m_state)
+    {
+      case ColorStateEnum::FADE:
+      {
+        const auto done = m_fade.update(current_time, led);
+        if (done) {
+          m_state = ColorStateEnum::IDLE;
+        }
+        break;
       }
-      return m_active;
+      case ColorStateEnum::FOLLOW:
+      {
+        m_follow.update(current_time, led);
+        break;
+      }
+      default:
+      {
+        Serial.println("Error: Invalid color state");
+      }
+      case ColorStateEnum::IDLE:
+      {
+        break;
+      }
     }
+    return m_state;
+  }
 
-    void clear() {
-      m_active = false;
-    }
+  void start_fade_state(
+      const millis_t init_time,
+      const millis_t delay_duration,
+      const millis_t fade_duration,
+      CRGB& led,
+      const uint8_t target_red,
+      const uint8_t target_green,
+      const uint8_t target_blue) {
+    m_state = ColorStateEnum::FADE;
+    m_fade.set(init_time, delay_duration, fade_duration, led, target_red, target_green, target_blue);
+  }
 
-  private: // helper methods
-    // This can change how the transition curve looks. Currently it's linear.
-    double transform_progress(const double progress) {
-      return progress;
-    }
+  FadeState m_fade;
+  FollowState m_follow;
+  ColorStateEnum m_state;
+};
 
-  private: // members
-    bool m_active;
-
-    CRGB m_base_color;
-    CRGB m_target_color;
-
-    int m_r_offset;
-    int m_b_offset;
-    int m_g_offset;
-
-    millis_t m_start_time;
-    millis_t m_duration;
+enum class StateManagerEnum {
+  IDLE,
+  UNIFORM_RANDOM,
+  EACH_RANDOM,
 };
 
 template<size_t NUM_LEDS>
-struct FadeManager {
+struct StateManager {
   public: // ctors
-    FadeManager(array_t<CRGB, NUM_LEDS>& leds) : m_leds(leds), m_faders() {}
+    StateManager(array_t<CRGB, NUM_LEDS>& leds)
+    : m_state(StateManagerEnum::IDLE), m_leds(leds), m_states()
+    {}
 
   public: // api
     void set(
         const size_t i,
-        const millis_t start_time,
-        const millis_t duration,
+        const millis_t current_time,
+        const millis_t delay_duration,
+        const millis_t fade_duration,
         const uint8_t r,
         const uint8_t g,
         const uint8_t b) {
-      m_faders[i].set(start_time, duration, m_leds[i], r, g, b);
+      m_states[i].start_fade_state(current_time, delay_duration, fade_duration, m_leds[i], r, g, b);
     }
 
     void set(
-        const millis_t start_time,
-        const millis_t duration,
+        const millis_t current_time,
+        const millis_t delay_duration,
+        const millis_t fade_duration,
         const uint8_t r,
         const uint8_t g,
         const uint8_t b) {
-      for (size_t i = 0; i < NUM_LEDS; ++i) {
-        set(i, start_time, duration, r, g, b);
+      for (size_t i = 0; i < NUM_LEDS; ++i)
+      {
+        set(i, current_time, delay_duration, fade_duration, r, g, b);
       }
     }
 
-    void clear(const size_t i) {
-      m_faders[i].clear();
-    }
-
-    void clear() {
-      for (size_t i = 0; i < NUM_LEDS; ++i) {
-        clear(i);
-      }
-    }
-
-    bool update(const size_t i, const millis_t current_time) {
-      if (m_faders[i].is_active()) {
-        return m_faders[i].update(current_time, m_leds[i]);
-      } else {
-        return false;
-      }
-    }
-
-    bool update(const millis_t current_time) {
+    void update() {
+      const auto current_time = millis();
       bool any_active = false;
-      for (size_t i = 0; i < NUM_LEDS; ++i) {
-        if (update(i, current_time)) {
+      for (size_t i = 0; i < NUM_LEDS; ++i)
+      {
+        const auto state = m_states[i].update(current_time, m_leds[i]);
+        if (state != ColorStateEnum::IDLE) {
           any_active = true;
         }
       }
-      return any_active;
+      if (!any_active) {
+        switch (m_state) {
+          case StateManagerEnum::UNIFORM_RANDOM:
+          {
+            set(
+                millis(),
+                m_random_delay_duration,
+                m_random_fade_duration,
+                rand_color(),
+                rand_color(),
+                rand_color());
+            break;
+          }
+          case StateManagerEnum::EACH_RANDOM:
+          {
+            const auto current_time = millis();
+            for (size_t i = 0; i < NUM_LEDS; ++i)
+            {
+              set(
+                  i,
+                  current_time,
+                  m_random_delay_duration,
+                  m_random_fade_duration,
+                  rand_color(),
+                  rand_color(),
+                  rand_color());
+            }
+            break;
+          }
+          case StateManagerEnum::IDLE:
+          {
+
+          }
+          default:
+          {
+            break;
+          }
+        }
+      }
+    }
+
+    void start_uniform_random_fade_loop(const millis_t delay_duration, const millis_t fade_duration) {
+      m_state = StateManagerEnum::UNIFORM_RANDOM;
+      m_random_delay_duration = delay_duration;
+      m_random_fade_duration = fade_duration;
+    }
+
+    void start_each_random_fade_loop(const millis_t delay_duration, const millis_t fade_duration) {
+      m_state = StateManagerEnum::EACH_RANDOM;
+      m_random_delay_duration = delay_duration;
+      m_random_fade_duration = fade_duration;
+    }
+
+  private: // helper methods
+    uint8_t rand_color() {
+      return random(256);
     }
 
   private: // members
+    StateManagerEnum m_state;
     array_t<CRGB, NUM_LEDS>& m_leds;
-    array_t<Fader, NUM_LEDS> m_faders;
+    array_t<ColorState, NUM_LEDS> m_states;
+
+    // state members
+    millis_t m_random_delay_duration;
+    millis_t m_random_fade_duration;
 };
 
 template<size_t DATA_PIN, size_t NUM_LEDS>
 struct LedManager {
   public: // ctors
-    LedManager() : m_leds(), m_fade_manager(m_leds) {}
+    LedManager() : m_leds(), m_state_manager(m_leds) {}
 
   public: // api
     void setup() {
@@ -154,9 +265,7 @@ struct LedManager {
     }
 
     void loop() {
-      if (!m_fade_manager.update(millis())) {
-        fade_all_to_random(1000);
-      }
+      m_state_manager.update();
       FastLED.show();
     }
 
@@ -165,55 +274,7 @@ struct LedManager {
       Serial.println("Adding handles for LedManager");
     }
 
-  private: // helper methods
-    void fade_all_to(const millis_t duration, const uint8_t r, const uint8_t g, const uint8_t b) {
-      m_fade_manager.set(millis(), duration, r, g, b);
-    }
-
-    void fade_all_to_random(const millis_t duration) {
-      const uint8_t r = rand_color();
-      const uint8_t g = rand_color();
-      const uint8_t b = rand_color();
-      m_fade_manager.set(millis(), duration, r, g, b);
-    }
-
-    void set_to(const size_t i, const millis_t duration, const uint8_t r, const uint8_t g, const uint8_t b) {
-      m_leds[i].r = r;
-      m_leds[i].g = g;
-      m_leds[i].b = b;
-    }
-
-    void set_all_to(const millis_t duration, const uint8_t r, const uint8_t g, const uint8_t b) {
-      for (size_t i = 0; i < NUM_LEDS; ++i) {
-        set_to(i, duration, r, g, b);
-      }
-    }
-
-    void set_random_to(const size_t i, const millis_t duration, const uint8_t r, const uint8_t g, const uint8_t b) {
-      m_leds[i].r = rand_color();
-      m_leds[i].g = rand_color();
-      m_leds[i].b = rand_color();
-    }
-
-    void set_random_all_to(const millis_t duration, const uint8_t r, const uint8_t g, const uint8_t b) {
-      for (size_t i = 0; i < NUM_LEDS; ++i) {
-        set_to(i, duration, r, g, b);
-      }
-    }
-
-    uint8_t rand_color() {
-      return random(256);
-    }
-
-    void wait_for_fade() {
-      while (m_fade_manager.update(millis()))
-      {
-        FastLED.show();
-        yield();
-      }
-    }
-
   private: // members
     array_t<CRGB, NUM_LEDS> m_leds;
-    FadeManager<NUM_LEDS> m_fade_manager;
+    StateManager<NUM_LEDS> m_state_manager;
 };
