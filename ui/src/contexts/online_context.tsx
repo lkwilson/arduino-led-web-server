ac7986c167a035aef14b92983589b8ac865c8c28
Lucas Wilson <lkwilson96@gmail.com>
2021-02-26 19:45:51 -0700

    Online check interval increases

--- ui/src/contexts/online_context.tsx
+++ ui/src/contexts/online_context.tsx
@@ -2,10 +2,13 @@ import React, { createContext, useState, useEffect } from 'react';
 import { is_connected } from '../utils/api_calls';
 
 const OnlineContext = createContext(null);
+const one_second = 1000;
+const five_minutes = 5 * 60 * one_second;
 
 function OnlineContextProvider(props) {
   const { children } = props;
   const [online, set_online] = useState(true);
+  const [retry_timeout, set_retry_timeout] = useState(one_second);
 
   function refresh_online() {
     is_connected()
@@ -17,9 +20,16 @@ function OnlineContextProvider(props) {
   }
 
   useEffect(_ => {
-    const timer = setInterval(refresh_online, 1000);
+    const timer = setInterval(() => {
+      refresh_online();
+      const max_retry_timeout = online ? one_second : five_minutes;
+      const new_retry_timeout = Math.min(max_retry_timeout, retry_timeout * 2);
+      if (new_retry_timeout !== retry_timeout) {
+        set_retry_timeout(new_retry_timeout);
+      }
+    }, retry_timeout);
     return _ => clearInterval(timer);
-  }, []);
+  }, [retry_timeout, online]);
 
   const online_state = {
     online,
